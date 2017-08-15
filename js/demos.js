561257801a5a857de58a73ff4ba5e610b245a723
Arjun Viswanathan <arjunviswanathan@Arjuns-MacBook-Pro.local>
2017-08-15 23:13:22 +0530

    Fixed typed.js

--- js/demos.js
+++ js/demos.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     typeSpeed: 20,
     backSpeed: 20,
     startDelay: 1000,
-    loop: false,
+    loop: true,
     loopCount: Infinity,
     onComplete: function(self) { prettyLog('onComplete ' + self) },
     preStringTyped: function(pos, self) { prettyLog('preStringTyped ' + pos + ' ' + self); },
